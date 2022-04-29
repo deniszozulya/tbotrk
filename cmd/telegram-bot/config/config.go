a10bb1c4d872d6b20dc2c38758b7ae8279e87ded
Denis Zozulya <den.zozulya@gmail.com>
2022-04-29 09:33:30 +0300

    * web hook url changed

--- cmd/telegram-bot/config/config.go
+++ cmd/telegram-bot/config/config.go
@@ -1,28 +1,28 @@
 package config
 
 import (
-	"log"
+        "log"
 
-	"github.com/caarlos0/env/v6"
+        "github.com/caarlos0/env/v6"
 )
 
 type config struct {
-	BotSecret string `env:"BOT_SECRET" envDefault:"5166381858:AAH-cz97Iz-zpdSJoBGXQcZCupXPC7rGHms"`
-	APISecret string `env:"API_SECRET" envDefault:"ce939b6c-611d-47a1-90a6-5054c8492733"`
-	WHAddr string `env:"WEBHOOK_ADDRESS" envDefault:"https://bokovski.ru/work/tg-bot/index.html"`
-	Debug bool `env:"DEBUG"`
-	ServerAddress string `env:"SERVER_ADDRESS"`
-	CertFilepath string `env:"CERT_FILE" envDefault:"cert.pem"`
-	KeyFilepath string `env:"KEY_FILE" envDefault:"key.pem"`
+        BotSecret string `env:"BOT_SECRET" envDefault:"5166381858:AAH-cz97Iz-zpdSJoBGXQcZCupXPC7rGHms"`
+        APISecret string `env:"API_SECRET" envDefault:"ce939b6c-611d-47a1-90a6-5054c8492733"`
+        WHAddr string `env:"WEBHOOK_ADDRESS" envDefault:"https://delivery.rkeeper.ru/botback"`
+        Debug bool `env:"DEBUG"`
+        ServerAddress string `env:"SERVER_ADDRESS"`
+        CertFilepath string `env:"CERT_FILE" envDefault:"cert.pem"`
+        KeyFilepath string `env:"KEY_FILE" envDefault:"key.pem"`
 }
 
 func New() *config {
-	var c config
+        var c config
 
-	err := env.Parse(&c)
-	if err != nil {
-		log.Fatal(err)
-	}
+        err := env.Parse(&c)
+        if err != nil {
+                log.Fatal(err)
+        }
 
-	return &c
+        return &c
 }
